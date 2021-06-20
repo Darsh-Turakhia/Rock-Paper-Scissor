f1f3ce9f7894d061b093e12064155fb99469bfcf
Darsh-Turakhia-GNU <56975912+Darsh1561@users.noreply.github.com>
2021-06-20 09:30:19 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -11,24 +11,34 @@ player = False
 
 while player == False:
     computer = t[randint(0,8)]
-    player = input("\n Rock, Paper, Scissors? ")
+    player = input("\n Rock, Paper, Scissors ? (Type q to exit) ")
     if player == computer:
         print("Tie!")
+        player = False
     elif player == "Rock":
         if computer == "Paper":
             print("You lose!", computer, "covers", player)
+            player = False
         else:
             print("You win!", player, "smashes", computer)
+            player = False
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cut", player)
+            player = False
         else:
             print("You win!", player, "covers", computer)
+            player = False
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose...", computer, "smashes", player)
+            player = False
         else:
             print("You win!", player, "cut", computer)
+            player = False
+    elif player == "q":
+        print("\n Exiting .... ")
+        player = True
     else:
         print("That's not a valid play. Check your spelling!")
-    player = False
+        player = False
